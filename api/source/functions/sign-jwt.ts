db9ffb2631b3e182274cccf43f9e735ae84c5402
Kethableez <mocarto98@gmail.com>
2022-02-26 01:07:48 +0100

    Added EsLint

--- api/source/functions/sign-jwt.ts
+++ api/source/functions/sign-jwt.ts
@@ -1,4 +1,3 @@
-import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../config/config';
 import logging from '../config/logging';
@@ -10,9 +9,10 @@ const signJWT = (
     user: IUser,
     callback: (error: Error | null | unknown, token: string | null) => void
 ): void => {
-    var timeSinceEpoch = new Date().getTime();
-    var expirationTime = timeSinceEpoch + Number(config.server.token.expireTime) * 100000;
-    var expirationTimeInSec = Math.floor(expirationTime / 1000);
+    const timeSinceEpoch = new Date().getTime();
+    const expirationTime =
+        timeSinceEpoch + Number(config.server.token.expireTime) * 100000;
+    const expirationTimeInSec = Math.floor(expirationTime / 1000);
 
     logging.info(NAMESPACE, `Attempt to sign token for ${user.username}`);
     try {
